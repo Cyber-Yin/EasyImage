0cf37f92f8c5bf0ae70fd5d1d4ed5764d9f248e9
CyberYin <cyberyin.me@gmail.com>
2024-07-18 14:46:32 +0800

    chore: 构建镜像

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import fs from "fs";
 
 config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 const TOKEN = process.env.TOKEN || "image_token";
 
 const uploadDir = path.join(__dirname, "images");
